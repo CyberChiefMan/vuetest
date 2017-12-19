be14b37218f98a4522fadb686fe1378ac2a680ec
m <mahmoudi.m2@gmail.com>
2017-12-20 01:02:00 +0330

    960928

--- src/app.vue
+++ src/app.vue
@@ -185,7 +185,7 @@
                 *این فیلتر روی تمامی تکالیف قانونی سایت اعمال می شود
                 <button class="btn btn-link" style="color: rgba(95, 95, 95, 1);">حذف فیلتر</button>
                 <button class="btn btn-sucsess"
-                  style="background-color: rgba(3, 169, 244, 1);
+                        style="background-color: rgba(3, 169, 244, 1);
                   border-radius: 15px;color: white;">
                   اعمال فیلتر
                 </button>
@@ -204,7 +204,7 @@
 
       <div id="sumery" style="background-color: rgba(3, 169, 244, 1);color:rgba(255, 255, 255, 1); text-align: center">
         <div class="row">
-<div class="col-md-2"></div>
+          <div class="col-md-2"></div>
           <div class="col-md-2">
             <h2>۱۱۱</h2>
             <h5 class="subtitle"> تکلیف قانونی ثبت شده</h5>
@@ -266,48 +266,53 @@ color: white">
 
       </div>
 
-      <div id="contactUs" style="background-color: black;color: white;background-image:url('/src/assests/contactus.jpg');background-repeat: no-repeat;padding: 7px;">
-      <div class="row">
-        <div class="col-md-2">
-          <div style="display: inline-block;vertical-align: middle;float: none;">
-            <h5><img src="./assests/customer_service_icon.svg" alt="telIcon">تلفن تماس: </h5>
-            <h6> ۰۲۱ ۳۲۳۴ ۲۴۴ ۴ </h6>
-
-            <h5><img src="./assests/mail_icon.svg" alt="telIcon">پست الکترنیک: </h5>
-            <h6> Info@Jamnevisi.ir </h6>
-          </div>
-        </div>
-        <div class="col-md-2">
-          <!--just for offset-->
-        </div>
-        <div class="col-md-6" style="padding-top: 20px;">
-          <div >
-
-            <input type="text" placeholder="نام و نام خانوادگی" aria-describedby="basic-addon1" class="contactUsText">
-            <br>
-            <br>
-            <input type="tel" placeholder=" موبایل " aria-describedby="basic-addon1" class="contactUsText">
-            <input type="email" placeholder="پست الکترونیک" aria-describedby="basic-addon1" class="contactUsText">
-            <br>
-            <br>
-            <textarea name="comment" placeholder="پیغام خود را اینجا بنویسید" cols="30" rows="10" style="background-color: inherit;border-color:transparent;"></textarea>
-            <br>
+      <div id="contactUs" style=" background-color: black;color: white;background-image:url('/src/assests/contactus.jpg');background-repeat: no-repeat;background-size:auto; padding: 7px;">
+        <div class="row">
+          <div style="text-align: center">
+            <h4>ارتباط با ما</h4>
+            <h6>در قانون‌بان، همیشه منتظر پیشنهادات شما هستیم</h6>
           </div>
 
-          <div style="text-align: left">
-            <button class="btn " style="top: 28px;
+          <div class="col-md-2">
+            <div style="text-align: center;padding: 20px">
+              <h6><img src="./assests/customer_service_icon.svg" alt="telIcon">تلفن تماس: </h6>
+              <h6> ۰۲۱ ۳۲۳۴ ۲۴۴ ۴ </h6>
+
+              <h6><img src="./assests/mail_icon.svg" alt="telIcon">پست الکترنیک:</h6>
+              <h6> Info@Jamnevisi.ir </h6>
+            </div>
+          </div>
+          <div class="col-md-2">
+            <!--just for offset-->
+          </div>
+          <div class="col-md-6" style="padding-top: 20px;">
+            <div >
+
+              <input type="text" placeholder="نام و نام خانوادگی" aria-describedby="basic-addon1" class="contactUsText">
+              <br>
+              <br>
+              <input type="tel" placeholder=" موبایل " aria-describedby="basic-addon1" class="contactUsText">
+              <input type="email" placeholder="پست الکترونیک" aria-describedby="basic-addon1" class="contactUsText">
+              <br>
+              <br>
+              <textarea name="comment" placeholder="پیغام خود را اینجا بنویسید" cols="30" rows="10" style="background-color: inherit;border-color:transparent;"></textarea>
+              <br>
+            </div>
+
+            <div style="text-align: left">
+              <button class="btn " style="top: 28px;
                     left: 93px;
                     background-color: rgb(41, 182, 246);
                     border-radius: 18px;
                     color: white;
 ">ارسال پیام
-            </button>
+              </button>
+            </div>
+
+
           </div>
-
-
+          <div class="col-md-2"></div>
         </div>
-        <div class="col-md-2"></div>
-      </div>
       </div>
 
       <div id="mapAdress" style="background-image:url('/src/assests/acf-google-map-field-input.png');background-color: rgba(179, 229, 252, 1);color: black">
@@ -323,7 +328,7 @@ color: white">
         </div>
       </div>
 
-      <div class="footer" id="footer" style="background-color: rgba(74, 74, 74, 1);text-align:right;padding-right: 10px;">
+      <div id="footer" class="footer"  style="background-color: rgba(74, 74, 74, 1);text-align:right;padding-right: 10px;">
         <div class="row">
           <div class="col-md-3">
             <h3>قانون‌بان در رسانه ها</h3>
@@ -381,10 +386,50 @@ color: white">
           <div class="col-md-5">
             <h3>آخرین نظرات ثبت شده در قانون ها</h3>
             <hr>
-
+            <div>
+              <h6 style="text-align: right;color: rgba(41, 182, 246, 1);">دسترسی باز به اطلاعات</h6>
+              <h6 style="text-align: left;color: rgba(41, 182, 246, 1);">۲۴ مهر ۱۳۹۶ </h6>
+              <p>
+                میتونه خلاصه ای از آخرین اصلاحیه یا کامنتی که کاربران درباره این قانون نوشتن رو اینجا نشون بده، در واقع این بخش برای ایجاد سئو و.. در فوتر گذاشته می شود
+              </p>
+            </div>
+            <div>
+              <h6 style="text-align: right;color: rgba(41, 182, 246, 1);">نام قانون در این بخش نمایش داده می شود</h6>
+              <h6 style="text-align: left;color: rgba(41, 182, 246, 1);">۲۴ مهر ۱۳۹۶ </h6>
+              <p>
+                میتونه خلاصه ای از آخرین اصلاحیه یا کامنتی که کاربران درباره این قانون نوشتن رو اینجا نشون بده، در واقع این بخش برای ایجاد سئو و.. در فوتر گذاشته می شود              </p>
+            </div>
+            <div>
+              <h6 style="text-align: right;color: rgba(41, 182, 246, 1);">عنوان ها ی درج شده مختلف هستند</h6>
+              <h6 style="text-align: left;color: rgba(41, 182, 246, 1);">۲۴ مهر ۱۳۹۶ </h6>
+              <p>
+                میتونه خلاصه ای از آخرین اصلاحیه یا کامنتی که کاربران درباره این قانون نوشتن رو اینجا نشون بده، در واقع این بخش برای ایجاد سئو و.. در فوتر گذاشته می شود              </p>
+            </div>
           </div>
         </div>
 
+      </div>
+
+      <div id="copywrite" class="row">
+        <div class="col-md-2">
+<h6><img src='./assests/go_to_top_icon.svg' alt="">ابتدای صفحه</h6>
+        </div>
+        <div class="col-md-2">
+<!--just for offset-->
+        </div>
+<div class="col-md-5">
+<h6>
+  تمامی حقوق این وب سایت متعلق به مجموعه قانون‌بان می باشد
+</h6>
+  © 2017 all Rights Reserved by GhanoonBaan
+
+</div>
+        <div class="col-md-1"></div>
+        <div class="col-md-2">
+<button class="btn-copywrite"><img src='./assests/twitter_logo.svg' alt=""></button>
+<button class="btn-copywrite"><img src='./assests/aparat_logo.svg' alt=""></button>
+<button class="btn-copywrite"><img src='./assests/telegram_logo.svg' alt=""></button>
+        </div>
       </div>
 
     </div> <!--container-end-->
@@ -444,5 +489,16 @@ color: white">
   .contactUsText{
     background-color: inherit;
     border-color: transparent
+  }
+ #copywrite{
+  background-color: rgba(33, 150, 243, 1);
+   text-align:center;
+   padding-top:5px;
+  }
+  .btn-copywrite{
+    text-align: center;
+    background-color: inherit;
+    border-color: transparent;
+    padding: 10px;
   }
 </style>
