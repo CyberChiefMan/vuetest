e40e2bed9afbedd30474b33b2014793903463315
m <mahmoudi.m2@gmail.com>
2017-12-26 00:24:04 +0330

    961005_0000

--- src/components/‌Banner.vue
+++ src/components/‌Banner.vue
@@ -1,21 +1,23 @@
 <template>
-        <div id="banner"  style="background-color: rgba(3, 169, 244, 1);">
+        <div id="banner"  style="">
           <div class="row">
             <div class="col-lg-3"></div>
             <div class="col-lg-6">
               <div style="
                     padding-top: 30px;
-                    left: 0px;
+                    padding-bottom: 30px;
+                    /*left: 0px;
                     height: 248px;
-                    width: 100%;
-                    background-color: rgba(3, 169, 244, 1);">
+                    width: 100%;*/
+                   ">
                 <!--<img src="./assests/" alt="">-->
                 <div id="banner-content" style="color: rgba(255, 255, 255, 1);text-align: center">
-                  <h1> قانون‌بان </h1>
-                  <small>سامانه رصد تکالیف قانونی زمان‌دار</small>
+                  <h9> قانون‌بان </h9>
+                  <br>
+                  <textSize18>سامانه رصد تکالیف قانونی زمان‌دار</textSize18>
                   <br>
                   <br>
-                  <p>
+                  <p style="font-size: 24px">
                     سایت قوانین زماندار درصدد است با جمع آوری مصادیق این قوانین و اعلام فرصت باقی مانده برای اجرای آنها و
                     یا
                     احیانا زمانی که از اجرای آنها گذشته است، افکار عمومی را نسبت به اجرایی شدن این قوانین حساس کرده و
@@ -44,10 +46,11 @@
   }
 </script>
 <style scoped>
-  {
-  
-
-
-  }
-
+#banner{
+  background-color: rgba(3, 169, 244, 1);
+}
+h1{
+  font-size: 48px;
+  font-weight: bold;
+}
 </style>
